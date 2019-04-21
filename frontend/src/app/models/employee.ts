741f9fba403eba268acdd811040b174da3372db9
Christian Mena <christian.91.gm@hotmail.com>
2019-04-20 23:08:01 -0500

    Crud Stack MEAN

---
+++ frontend/src/app/models/employee.ts
@@ -0,0 +1,26 @@
+export class Employee {
+
+     //variables
+     _id: string;
+     name: string;
+     position: string;
+     office: string;
+     salary: number;
+    
+
+
+    //constructor
+    constructor(_id = '', name = '', position= '', office= '', salary= 0) {
+        this._id = _id;
+        this.name = name;
+        this.position = position;
+        this.office = office;
+        this.salary = salary;
+
+    }
+
+   
+
+
+
+}
